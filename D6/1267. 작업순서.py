e0e36e2e5a8a76729e084a8cea4070a506c1d25d
gusghk <h3652k@gmail.com>
2019-08-20 09:30:26 +0900

    8/20

--- D6/1267. 작업순서.py
+++ D6/1267. 작업순서.py
@@ -1,46 +1,24 @@
-for t in range(1):
+# import sys
+# sys.stdin = open('input.txt', 'r')
+
+for t in range(10):
     V, E = map(int, input().split())
     e = list(map(int, input().split()))
-    e = [e[i:i+2] for i in range(0,len(e),2)]
-    road = [False for i in range(V)]
-    checklist = []
-    for i in e:
-        if not checklist:
-            checklist.append(i)
-        elif checklist[-1][0] == i[1]:
-            checklist.pop(-1)
-            checklist.append(i)
-    e.remove(checklist[-1])
-    checklist = checklist[0]
-    for i in checklist:
-        road[i-1] = True
-    while True:
-        for i in e:
-            if checklist[-1] == i[0] and road[i[1]-1] == False:
-                checklist.append(i[1])
-                road[i[1]-1] = True
-                for j in e:
-                    if j != i and i[1] == j[1]:
-                        checklist.remove(i[1])
-                        road[i[1]-1] = False
-                        break
-            elif checklist[-1] != i[0] and road[i[1]-1] == False:
-                if i[0] not in checklist:
-                    checklist.append(i[0])
-                    road[i[0]-1] = True
-                checklist.append(i[1])
-                road[i[1]-1] = True
-                for j in e:
-                    if j != i and i[1] == j[1] and i[0] == j[1] and road[j[1]-1] == False:
-                        if checklist[-2] == i[0]:
-                            checklist.remove(i[0])
-                            road[i[0]-1] = False
-                        checklist.remove(i[1])
-                        road[i[1]-1] = False
-                        break
-        if False not in road:
-            break
-
-                
-
-    print(checklist)
+    dfs = [0 for i in range(1, V+1)]
+    visited = []
+    graph = [[] for i in range(V)]
+    s = set()
+    for i in range(0, len(e), 2):
+        graph[e[i]-1].append(e[i+1]) # [[2, 5], [3, 7], [], [1], [6], [], [6], [5, 9], []]
+        dfs[e[i+1]-1] += 1
+        s.update({e[i+1]})
+        stack = {i for i in range(1, V+1)}.difference(s)
+    stack = list(stack)
+    while stack:
+        node = stack.pop()
+        if dfs[node-1]:
+            dfs[node-1] -= 1
+        if dfs[node-1] == 0:
+            visited.append(node)
+            stack.extend(graph[node-1])
+    print('#{} {}'.format(t+1, ' '.join(list(map(str, visited)))))
