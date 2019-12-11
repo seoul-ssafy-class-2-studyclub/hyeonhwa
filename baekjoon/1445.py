e33c1fa4c94f0f074654ccd4e43b08337769410e
gusk <h3652k@gmail.com>
2019-12-11 17:51:58 +0900

    12/11

--- baekjoon/1445.py
+++ baekjoon/1445.py
@@ -1,10 +1,13 @@
+# from heapq import heappush, heappop
+
 def go():
     pass
+
 
 N, M = map(int, input().split())
 board = [list(input()) for _ in range(N)]
 idx = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-trash = [[0]*N for _ in range(M)]
+ht = [[[0, 0] for _ in range(N)] for __ in range(M)]
 neartrash = [[0]*N for _ in range(M)]
 for x in range(N):
     for y in range(M):
@@ -13,7 +16,6 @@ for x in range(N):
         elif board[x][y] == 'F':
             end = (x, y)
         elif board[x][y] == 'g':
-            trash[x][y] = 1
             for dx, dy in idx:
                 if 0 <= x+dx < N and 0 <= y+dy < M:
                     neartrash[x+dx][y+dy] = 1
