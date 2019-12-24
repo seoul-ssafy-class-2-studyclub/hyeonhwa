2098d059d9995991718a16605a8037ac20395586
gusk94 <h3652k@gmail.com>
2019-12-24 09:34:32 +0900

    finish 18235

--- baekjoon/18235_지금만나러갑니다.py
+++ baekjoon/18235_지금만나러갑니다.py
@@ -1,34 +1,44 @@
+from collections import deque
+
+def meet():
+    queue1 = deque([A])
+    queue2 = deque([B])
+    i = 1
+    cnt = 0
+    while queue1 and queue2:
+        if len(queue1) == 0 or len(queue2) == 0:
+            return -1
+        visit = [0]*(N+1)
+        for _ in range(len(queue1)):
+            x = queue1.popleft()
+            if x - i > 0 and not visit[x-i]:
+                queue1.append(x-i)
+                visit[x-i] = 1
+            if x + i <= N and not visit[x+i]:
+                queue1.append(x+i)
+                visit[x+i] = 1
+        for _ in range(len(queue2)):
+            y = queue2.popleft()
+            if y - i > 0:
+                if not visit[y-i]:
+                    queue2.append(y-i)
+                    visit[y-i] = 1
+                else:
+                    return cnt+1
+            if y + i <= N:
+                if not visit[y+i]:
+                    queue2.append(y+i)
+                    visit[y+i] = 1
+                else:
+                    return cnt+1
+        i *= 2
+        cnt += 1
+    return -1
+            
+
 N, A, B = map(int, input().split())
-fduck = [0]*(N*4)
-fduck[1] = A
-sduck = [0]*(N*4)
-sduck[1] = B
-res = 0
-k, i = 1, 1
-while i < 4*N:
-    if fduck[i] != 0:
-        if fduck[i] - k > 0:
-            fduck[i*2] = fduck[i] - k
-        if fduck[i] + k <= N:
-            fduck[i*2+1] = fduck[i] + k
-    if sduck[i] != 0:
-        if sduck[i] - k > 0:
-            sduck[i*2] = sduck[i] - k
-        if sduck[i] + k <= N:
-            sduck[i*2+1] = sduck[i] + k
-    i += 1
-    if i == k*2:
-        flag = 0
-        k *= 2
-        res += 1
-        for j in fduck[i:i*2]:
-            if j != 0 and j in sduck[i:i*2]:
-                flag = 1
-                break
-        if flag == 1:
-            break
-        if fduck[i:i*2].count(0) == k or sduck[i:i*2].count(0) == k:
-            res = -1
-            break
-print(sduck)
+if (A % 2 and not B % 2) or (not A % 2 and B % 2):
+    res = -1
+else:
+    res = meet()
 print(res)
