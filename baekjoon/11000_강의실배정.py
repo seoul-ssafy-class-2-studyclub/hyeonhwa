f8591b3276df98c1d49715579cbf266a6961b949
gusk94 <h3652k@gmail.com>
2020-05-20 21:44:15 +0900

    5/20 greedy priority queue(using heapq)

--- baekjoon/11000_강의실배정.py
+++ baekjoon/11000_강의실배정.py
@@ -1,17 +1,15 @@
 import sys
+import heapq
 input = lambda: sys.stdin.readline().rstrip()
 
 # 우선순위큐
 n = int(input())
 subject = [list(map(int,input().split())) for _ in range(n)]
 subject.sort(key=lambda x:x[0])
-finish = [subject[0][1]]
+finish = []
+heapq.heappush(finish, subject[0][1])
 for s, t in subject[1:]:
-    if finish[0] > s:
-        finish.append(t)
-    else:
-        finish.pop()
-        finish.append(t)
-print(finish)
-
-# https://covenant.tistory.com/129
+    if finish[0] <= s:
+        heapq.heappop(finish)
+    heapq.heappush(finish, t)
+print(len(finish))
