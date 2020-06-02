80582cfd54c71f4d7326136a10d7fd81d9ff9c61
gusk94 <h3652k@gmail.com>
2020-06-02 14:54:18 +0900

    6/2 memoization

--- baekjoon/1937_욕심쟁이판다.py
+++ baekjoon/1937_욕심쟁이판다.py
@@ -2,29 +2,22 @@ import sys
 from collections import deque
 input = lambda: sys.stdin.readline().rstrip()
 
-def bfs(i, j):
-    global res
-    # visit = [[0]*n for _ in range(n)]
-    # visit[i][j] = 1
-    queue = deque()
-    queue.append((i, j))
-    cnt = 1
-    while queue:
-        for _ in range(len(queue)):
-            x, y = queue.popleft()
-            for dx, dy in idx:
-                if 0 <= x+dx < n and 0 <= y+dy < n and forest[x+dx][y+dy] > forest[x][y]:
-                    queue.append((x+dx, y+dy))
-                    # visit[x+dx][y+dy] = 1
-        cnt += 1
-    res = max(res, cnt-1)
+def check(a, b):
+    if visit[a][b]:
+        return visit[a][b]
+    visit[a][b] = 1
+    for dx, dy in idx:
+        if 0 <= a+dx < n and 0 <= b+dy < n and forest[a][b] < forest[a+dx][b+dy]:
+            visit[a][b] = max(visit[a][b], check(a+dx, b+dy)+1)
+    return visit[a][b]
 
 
 n = int(input())
 forest = [[int(i) for i in input().split()] for _ in range(n)]
 idx = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-res = 0
-for i in range(n):
-    for j in range(n):
-        bfs(i, j)
-print(res)
+visit = [[0]*n for _ in range(n)]
+ans = 0
+for x in range(n):
+    for y in range(n):
+        ans = max(ans, check(x, y))
+print(ans)
--- 알고리즘 개념/SW 역량 테스트.md
+++ 알고리즘 개념/SW 역량 테스트.md
@@ -303,3 +303,34 @@ for t in range(int(input())):
 
 
 ### 하드코딩 -> index 주의
+
+
+
+### 메모이제이션
+
+```python
+# 욕심쟁이 판다
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def check(a, b):
+    if visit[a][b]:
+        return visit[a][b]
+    visit[a][b] = 1
+    for dx, dy in idx:
+        if 0 <= a+dx < n and 0 <= b+dy < n and forest[a][b] < forest[a+dx][b+dy]:
+            visit[a][b] = max(visit[a][b], check(a+dx, b+dy)+1)
+    return visit[a][b]
+
+
+n = int(input())
+forest = [[int(i) for i in input().split()] for _ in range(n)]
+idx = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+visit = [[0]*n for _ in range(n)]
+ans = 0
+for x in range(n):
+    for y in range(n):
+        ans = max(ans, check(x, y))
+print(ans)
+```
+
