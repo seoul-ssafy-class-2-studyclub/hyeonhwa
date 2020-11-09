f1e2513b6669e6b4ae9cdd6596c353ea219bd82e
gusk94 <h3652k@gmail.com>
2020-11-10 00:51:13 +0900

    11/10

---
+++ baekjoon/2110_공유기설치.py
@@ -0,0 +1,20 @@
+n, c = map(int, input().split())
+house = [int(input()) for _ in range(n)]
+house.sort()
+s = house[1]-house[0]
+e = house[-1]-house[0]
+res = 0
+while s < e:
+    mid = (s+e)//2
+    x = house[0]
+    cnt = 1
+    for i in range(1, n):
+        if house[i] - x >= mid:
+            cnt += 1
+            x = house[i]
+    if cnt >= c:
+        s = mid+1
+        res = mid
+    else:
+        e = mid
+print(res)
