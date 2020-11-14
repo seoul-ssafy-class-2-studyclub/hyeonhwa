add6e5c82219792f2814df7a3d48397d671b2b95
gusk94 <h3652k@gmail.com>
2020-11-15 00:14:56 +0900

    11/15

--- baekjoon/2751_수정렬하기2.py
+++ baekjoon/2751_수정렬하기2.py
@@ -1,4 +1,22 @@
+def quick(arr):
+    if len(arr) <= 1:
+        return arr
+    left = []
+    right = []
+    mid = []
+    pivot = arr[len(arr)//2]
+    for i in arr:
+        if i < pivot:
+            left.append(i) 
+        elif i > pivot:
+            right.append(i)
+        else:
+            mid.append(i)
+    return quick(left) + mid + quick(right)
+
 n = int(input())
 nums = [int(input()) for _ in range(n)]
-for i in sorted(nums):
+# for i in sorted(nums):
+#     print(i)
+for i in quick(nums):
     print(i)
